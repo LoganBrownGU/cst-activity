d069e1a2479f8aa30728f66e690649139e0c90ea
LoganBrownGU <2641407b@student.gla.ac.uk>
2025-02-19 23:57:48 +0000

    Added example test

---
+++ buggy-account-system/src/test/java/DatabaseTest.java
@@ -0,0 +1,12 @@
+import com.example.Database;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+    
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
